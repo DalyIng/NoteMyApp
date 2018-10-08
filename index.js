3b5edb879a729440697534fd2572c93c776dcc36
Masmoudi Mohamed Ali <mohammedali.masmoudi@gmail.com>
2018-10-08 23:09:26 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//commonJs modules for the backend, node don't support ES2016!
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({
+    hi: "there"
+  });
+}); 
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
